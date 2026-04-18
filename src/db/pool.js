285b8b896b09fcb29bb2366cab58d065e53d9f26
AKengg1 <ak72532961@gmail.com>
2026-04-18 13:08:05 +0530

    preparing db pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,18 +1,15 @@
 require('dotenv').config({ path: '../.env' });
-// require('dotenv').config()
-const { Pool } = require('pg');
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME     || 'signals_db',
-  user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD ,
-  max: 20,               // max connections in pool
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
+module.exports = pool;
 // Test connection on startup
 pool.connect((err, client, release) => {
   if (err) {
